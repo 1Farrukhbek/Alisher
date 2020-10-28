5017bcf538c5744561206eafc7e4b7b43855202f
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2020-10-28 10:44:36 +0500

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,22 @@
-// var info = ('ALISHER PULING BORMI O\'ZI?');
-// alert(info);
-// var currentMoney = prompt('QANCHA PULING BOR?').trim();
+var ticketCost = 500;
+var hotelCost = 250;
+var museumCost = 120;
+var USD_TO_UZS = 9433.34;
+var EURO_TO_UZS = 10354.03;
+
+ticketCost = Math.round(ticketCost * USD_TO_UZS);
+hotelCost = Math.round(hotelCost * USD_TO_UZS);
+museumCost = Math.round(museumCost * EURO_TO_UZS);
 
 
-// var massage = ('YOUR AMOUNT: ' + currentMoney);
-  
-// console.log(massage);
-// alert(massage);
+var totalExpence = ticketCost + hotelCost + museumCost;
+document.querySelector('.total-cost').textContent = (totalExpence + ' so\'m');
 
-var flight = 500;
-var hotel = 250;
-var museum = 120;
-var usd = 9433.34;
-var euro = 10354.03;
-
-flight = Math.round(flight * usd);
-hotel = Math.round(hotel * usd);
-museum = Math.round(museum * euro);
-
-
-var expence = flight + hotel + museum;
-document.querySelector('.total-cost').textContent = (expence + ' so\'m');
-
-var currentMoney = prompt('QANCHA PULING BOR?').trim();
+var currentMoney = parseInt(prompt('QANCHA PULING BOR?').trim());
 document.querySelector('.alisher-money').textContent = (currentMoney + ' so\'m');
 
-if (expence <= currentMoney) {
-  document.querySelector('.answer').textContent = ('ALISHER PULING YATADI, LIKIN JS DARSLARIDAN BARIBIR QOCHIB KETOLMAYSAN!')
+if (totalExpence <= currentMoney) {
+  document.querySelector('.answer').textContent = ('ALISHER PULING YETADI, LIKIN JS DARSLARIDAN BARIBIR QOCHIB KETOLMAYSAN!')
 
   var element = document.getElementById("yes");
   element.classList.add("d-flex");
